c9883e5fb21495fba694c680a957a5a6172c5c5d
stupid- <unknown666@gmail.com>
2015-02-16 13:21:39 -0500

    Update cl_hud.lua

--- gamemodes/tdm/gamemode/cl_hud.lua
+++ gamemodes/tdm/gamemode/cl_hud.lua
@@ -24,10 +24,42 @@ surface.CreateFont( "HeadTextShadow2", { font = "CloseCaption_Bold", size = 124,
 surface.CreateFont( "HeadTextShadow", { font = "CloseCaption_Bold", size = 124, weight = 600, antialias = true, blursize = 6, shadow = false })
 surface.CreateFont( "HeadText", { font = "CloseCaption_Bold", size = 124, weight = 600, antialias = true, blursize = 0, shadow = false })
 
--- Draw Teams and Scores on Bottom Right
-local function DrawTeams( ply )
+/*
+local health_colors = {
+    
+}
 
-    local round = self:GetRound()
+local team_colors = {
+    
+}
+*/
+
+-- Blur Function for BG Blurs
+local blur = Material("pp/blurscreen")
+local function DrawBlur( amount, x, y, w, h )
+    surface.SetDrawColor(255, 255, 255, 255)
+    surface.SetMaterial(blur)
+
+    for i = 1, 3 do
+        blur:SetFloat("$blur", (i / 3) * (amount or 6))
+        blur:Recompute()
+
+        render.UpdateScreenEffectTexture()
+        render.SetScissorRect( x, y, x + w, y + h, true )
+        surface.DrawTexturedRect( 0, 0, ScrW(), ScrH() )
+        render.SetScissorRect( 0, 0, 0, 0, false )
+    end
+end
+
+-- Different Gradients
+local gradient = surface.GetTextureID("gui/gradient_up") 
+local gradient2 = surface.GetTextureID("gui/gradient_down")
+local gradient3 = surface.GetTextureID("gui/gradient")
+local gradient4 = surface.GetTextureID("vgui/gradient-r")
+
+-- Draw Teams and Scores on Bottom Right
+local function DrawTeams( ply, roundState, roundTime, redKills, blueKills, scoreLimit, roundsLeft )
+    local round = roundState
 
     if round == 0 then round = "Waiting"
     elseif round == 1 then round = "Preparing"
@@ -35,11 +67,11 @@ local function DrawTeams( ply )
     elseif round == 3 then round = "Round Over"
     end
 
-    local time = string.ToMinutesSeconds(self:GetRoundTime())
-    local rk = self:GetRedKills()
-    local bk = self:GetBlueKills()
-    local sl = self:GetScoreLimit()
-    local left = self:GetRoundsLeft()
+    local time = roundTime
+    local rk = redKills
+    local bk = blueKills
+    local sl = scoreLimit
+    local left = roundsLeft
 
     local team1_deaths = bk * 10
     local team2_deaths = rk * 10
@@ -69,24 +101,20 @@ local function DrawTeams( ply )
     draw.DrawText( "B", "TimeLeft", ScrW()-279, ScrH() - 89 , Color(255, 255, 255, 255), TEXT_ALIGN_CENTER ) 
     draw.DrawText( "R", "TimeLeft", ScrW()-279, ScrH() - 124 , Color(255, 255, 255, 255), TEXT_ALIGN_CENTER ) 
 
-    self.Gradient = surface.GetTextureID("gui/gradient_down")
     surface.SetDrawColor( 0, 0, 0, 90 );
-    surface.SetTexture( self.Gradient );
+    surface.SetTexture( gradient2 );
     surface.DrawTexturedRect( ScrW()-292, ScrH() - 90, 232, 30 );
 
-    self.Gradient = surface.GetTextureID("gui/gradient_down")
     surface.SetDrawColor( 0, 0, 0, 90 );
-    surface.SetTexture( self.Gradient );
+    surface.SetTexture( gradient2 );
     surface.DrawTexturedRect( ScrW()-292, ScrH() - 125, 232, 30 );
 
-    self.Gradient = surface.GetTextureID("gui/gradient_up")
     surface.SetDrawColor(  255, 255, 255, 1  );
-    surface.SetTexture( self.Gradient );
+    surface.SetTexture( gradient );
     surface.DrawTexturedRect( ScrW()-292, ScrH() - 75, 232, 15 );
 
-    self.Gradient = surface.GetTextureID("gui/gradient_up")
     surface.SetDrawColor(  255, 255, 255, 1  );
-    surface.SetTexture( self.Gradient );
+    surface.SetTexture( gradient );
     surface.DrawTexturedRect( ScrW()-292, ScrH() - 110, 232, 15 );  
 
     draw.DrawText( team1_deaths, "ScoreShadow", ScrW()-65 + 1, ScrH() - 87 + 1 , Color(0, 0, 0, 255), TEXT_ALIGN_RIGHT ) 
@@ -103,13 +131,148 @@ local function DrawTeams( ply )
 end
 
 -- Draw XP Bar on the bottom of screen
-local function DrawXP()
+local function DrawXP( ply )
+
+    for i = 0, 10 do
+        draw.RoundedBox( 0, 57 + ( (ScrW() - 117) /10 * i ) , ScrH() - 45, 2, 13, Color( 255, 255, 255, 60) )
+    end     
+    
+    for i = 0,40 do
+
+        if ( i != 0 && i != 4 && i != 8 && i!= 12 && i != 16 && i != 20 && i != 24 && i != 28 && i != 32 && i != 36 && i != 40) then
+            draw.RoundedBox( 0, 57 + ( (ScrW() - 117) /40 * i ) , ScrH() - 41, 2, 6, Color( 255, 255, 255, 20) )
+        end
+    
+    end     
+
+    local prec = ply:GetNWInt( "Level_xp", 0 ) / ply:GetNWInt("Level_xp_max", 100)
+    prec = math.Clamp( prec, 0, 1 )
+
+    surface.SetDrawColor( 255, 225, 75, 140 );
+    surface.SetTexture( gradient4 );
+    surface.DrawTexturedRect( 57, ScrH() - 43, (ScrW() - 117) * prec, 9);
+
+    surface.SetDrawColor( 255, 255, 255, 4 );
+    surface.SetTexture( gradient );
+    surface.DrawTexturedRect( 57, ScrH() - 40, ScrW() - 117, 8);
+     
+    surface.SetDrawColor( 0, 0, 0, 40 );
+    surface.SetTexture( gradient2 );
+    surface.DrawTexturedRect( 57, ScrH() - 45, ScrW() - 117, 13 );
 
 end
 
 -- Draw HUD health, stamina and ammo/weapon
-local function DrawHUD()
+local function DrawHUD( ply )
+    local playerTeam = team.GetName( ply:Team() )
+    local playerClass = player_manager.GetPlayerClass( ply )
 
+    local prec = ply:Health() / ply:GetMaxHealth()
+    prec = math.Clamp( prec, 0, 1 )
+    draw.RoundedBox( 0, 57, ScrH() - 150, 306 * prec, 15, Color(210,50,50,240) )
+
+    surface.SetDrawColor( 255, 255, 255, 2 );
+    surface.SetTexture( gradient );
+    surface.DrawTexturedRect( 57, ScrH() - 145, 306, 10);
+
+    surface.SetDrawColor( 0, 0, 0, 90 );
+    surface.SetTexture( gradient2 );
+    surface.DrawTexturedRect( 57, ScrH() - 150, 306, 15 );
+
+    --Stamina Bar
+    local prec = ply:GetNWInt( "Stamina", 0 ) / 100 
+    prec = math.Clamp( prec, 0, 1 )
+    draw.RoundedBox( 0, 57, ScrH() - 135, 306 * prec, 3, Color( 255, 225, 75, 240) )
+
+    --If weapon equipped, display box and weapon name
+    if ply:GetActiveWeapon() != NULL then
+
+        DrawBlur( 0, 57, ScrH() - 125, 306, 65 )
+
+        if playerTeam == "Red" then
+
+            surface.SetDrawColor(175,20,20, 10 );
+            surface.SetTexture( gradient );
+            surface.DrawTexturedRect(0, ScrH()-100, ScrW(), 200);
+
+            surface.SetDrawColor(175,20,20, 10 );
+            surface.SetTexture( gradient2 );
+            surface.DrawTexturedRect(0, -100, ScrW(), 200);
+
+            surface.SetDrawColor( 175,20,20, 90 );
+            surface.SetTexture( gradient2 );
+            surface.DrawTexturedRect( 60, ScrH() - 125, 300, 65 );
+            draw.RoundedBox( 0 , 57, ScrH() - 125, 3, 65, Color(255,255,255,35) )
+            draw.RoundedBox( 0 , 360, ScrH() - 125, 3, 65, Color(255,255,255,35) )     
+            
+            surface.SetDrawColor( 255, 170, 170, 8 );
+            surface.SetTexture( gradient );
+            surface.DrawTexturedRect( 60, ScrH() - 95, 300, 35 );
+
+        elseif playerTeam == "Blue" then
+
+            surface.SetDrawColor(20,20,175, 10 );
+            surface.SetTexture( gradient );
+            surface.DrawTexturedRect(0, ScrH()-100, ScrW(), 200);
+
+            surface.SetDrawColor(20,20,175, 10 );
+            surface.SetTexture( gradient2 );
+            surface.DrawTexturedRect(0, -100, ScrW(), 200);
+
+            surface.SetDrawColor( 20,20,175, 90 );
+            surface.SetTexture( gradient2 );
+            surface.DrawTexturedRect( 60, ScrH() - 125, 300, 65 );
+            draw.RoundedBox( 0 , 60, ScrH() - 125, 3, 65, Color(255,255,255,35) )
+            draw.RoundedBox( 0 , 360, ScrH() - 125, 3, 65, Color(255,255,255,35) )
+            
+            surface.SetDrawColor( 170, 170, 255, 8 );
+            surface.SetTexture( gradient );
+            surface.DrawTexturedRect( 60, ScrH() - 95, 300, 35 );
+
+        end
+
+        local weaponName = ply:GetActiveWeapon():GetPrintName()
+        draw.DrawText( weaponName, "WeaponShadow", 72 + 1, ScrH() - 122 + 1, Color(0, 0, 0, 255), TEXT_ALIGN_LEFT ) 
+        draw.DrawText( weaponName, "Weapon", 72, ScrH() - 122, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT ) 
+
+        local commandText = "[F1] Switch Teams - [F2] Change Classes"
+        draw.DrawText( commandText, "CommandsShadow", 60 + 11, ScrH()-78 + 1, Color(0, 0, 0, 255), TEXT_ALIGN_LEFT ) 
+        draw.DrawText( commandText, "Commands", 60 + 10, ScrH()-78 , Color(255, 255, 255, 255), TEXT_ALIGN_LEFT ) 
+
+    end
+
+    -- Display Ammo
+    if ( IsValid( LocalPlayer():GetActiveWeapon() ) ) then
+        local ammoLeft = LocalPlayer():GetActiveWeapon():Clip1()
+        local ammoTotal = LocalPlayer():GetAmmoCount(LocalPlayer():GetActiveWeapon():GetPrimaryAmmoType())
+
+        if ammoLeft != -1 then
+
+            draw.RoundedBox( 0 , 295, ScrH() - 120, 2, 35, Color(255,255,255,10) )
+
+            draw.DrawText( ammoTotal, "AmmoSmallShadow", 326 + 1, ScrH() - 121 + 1, Color(0, 0, 0, 250), TEXT_ALIGN_CENTER ) 
+            draw.DrawText( ammoTotal, "AmmoSmall", 326, ScrH() - 121, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER ) 
+
+            draw.DrawText( ammoLeft, "AmmoLargeShadow", 260 + 1, ScrH() - 126 + 1, Color(0, 0, 0, 255), TEXT_ALIGN_CENTER ) 
+            draw.DrawText( ammoLeft, "AmmoLarge", 260, ScrH() - 126, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER ) 
+
+        end
+    end
+
+end
+
+function NamesOverPlayers()
+    if LocalPlayer():Team() == TEAM_SPEC then 
+        for k, v in pairs(player.GetAll()) do
+            if v:IsValid() && v:Alive() && v:Team() != TEAM_SPEC then
+                local pos = (v:GetPos() + Vector(0,0,80)):ToScreen()
+
+                draw.SimpleText( v:Nick(), "CommandsBlur", pos.x + 1, pos.y + 1, team.GetColor(v:Team()), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+                draw.SimpleText( v:Nick(), "CommandsShadow", pos.x + 1, pos.y + 1, Color( 0, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+                draw.SimpleText( v:Nick(), "Commands", pos.x, pos.y, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )              
+            end
+        end
+    end
 end
 
 function GM:HUDPaint()
@@ -124,7 +287,16 @@ function GM:HUDPaint()
 
     local version = GAMEMODE.Version
 
-    DrawTeams( ply )
+    local round = self:GetRound()
+    local time = string.ToMinutesSeconds(self:GetRoundTime())
+    local rk = self:GetRedKills()
+    local bk = self:GetBlueKills()
+    local sl = self:GetScoreLimit()
+    local left = self:GetRoundsLeft()
+
+    NamesOverPlayers()
+
+    DrawTeams( ply, round, time, rk, bk, sl, left )
 
 	--draw.DrawText( "Alpha " .. version, "AlphaFontShadow", x - 65 + 1, y/6 - 40 + 1, Color(10, 10, 10, 210), TEXT_ALIGN_RIGHT ) 
 	--draw.DrawText( "Alpha " .. version, "AlphaFont", x - 65, y/6 - 40, Color(255, 255, 255, 235), TEXT_ALIGN_RIGHT ) 
@@ -134,161 +306,21 @@ function GM:HUDPaint()
 
 	if LocalPlayer():Team() == TEAM_SPEC then 
 
-        --Commands so Spectators don't get stuck
+        --Hud for Spectators
+
         --local commandText = "[F1] Switch Teams"
         --draw.DrawText( commandText, "CommandsShadow", 60 + 11, ScrH()-78 + 1, Color(0, 0, 0, 255), TEXT_ALIGN_LEFT ) 
         --draw.DrawText( commandText, "Commands", 60 + 10, ScrH()-78 , Color(255, 255, 255, 255), TEXT_ALIGN_LEFT ) 
 
 	else
 
+        DrawXP( ply )
+
         --No Hud if dead
 		if (ply:Alive()) then
 
-            for i = 0, 10 do
-            
-                draw.RoundedBox( 0, 60 + ( (ScrW() - 122) /10 * i ) , ScrH() - 45, 2, 15, Color( 255, 255, 255, 60) )
-            
-            end     
-            
-            local count = 0
-            
-            for i = 0,40 do
-                count = count + 1
-            
-                if ( i != 0 && i != 4 && i != 8 && i!= 12 && i != 16 && i != 20 && i != 24 && i != 28 && i != 32 && i != 36 && i != 40) then
-            
-                    draw.RoundedBox( 0, 60 + ( (ScrW() - 121) /40 * i ) , ScrH() - 41, 2, 8, Color( 255, 255, 255, 20) )
-                    
-                end
-            
-            end     
+            DrawHUD( ply )
 
-            local prec = ply:GetNWInt( "Level_xp", 0 ) / ply:GetNWInt("Level_xp_max", 100)
-            prec = math.Clamp( prec, 0, 1 )
-            
-            self.Gradient = surface.GetTextureID("vgui/gradient-r")
-            surface.SetDrawColor( 255, 225, 75, 140 );
-            surface.SetTexture( self.Gradient );
-            surface.DrawTexturedRect( 60, ScrH() - 43, (ScrW() - 120) * prec, 11);
-             
-            self.Gradient = surface.GetTextureID("gui/gradient_up")
-            surface.SetDrawColor( 255, 255, 255, 4 );
-            surface.SetTexture( self.Gradient );
-            surface.DrawTexturedRect( 60, ScrH() - 40, ScrW() - 120, 10);
-             
-            self.Gradient = surface.GetTextureID("gui/gradient_down")
-            surface.SetDrawColor( 0, 0, 0, 40 );
-            surface.SetTexture( self.Gradient );
-            surface.DrawTexturedRect( 60, ScrH() - 45, ScrW() - 120, 15 );
-
-
-	    	local playerTeam = team.GetName( ply:Team() )
-	    	local playerClass = player_manager.GetPlayerClass( ply )
-
-
-	    	local prec = ply:Health() / ply:GetMaxHealth()
-	    	prec = math.Clamp( prec, 0, 1 )
-	    	draw.RoundedBox( 0, 57, ScrH() - 150, 306 * prec, 15, Color(210,50,50,240) )
-
-			self.Gradient = surface.GetTextureID("gui/gradient_up")
-			surface.SetDrawColor( 255, 255, 255, 2 );
-			surface.SetTexture( self.Gradient );
-			surface.DrawTexturedRect( 57, ScrH() - 145, 306, 10);
-
-			self.Gradient = surface.GetTextureID("gui/gradient_down")
-			surface.SetDrawColor( 0, 0, 0, 90 );
-			surface.SetTexture( self.Gradient );
-			surface.DrawTexturedRect( 57, ScrH() - 150, 306, 15 );
-
-            --Stamina Bar
-
-            local prec = ply:GetNWInt( "Stamina", 0 ) / 100 
-            prec = math.Clamp( prec, 0, 1 )
-            draw.RoundedBox( 0, 57, ScrH() - 135, 306 * prec, 3, Color( 255, 225, 75, 240) )
-
-            --If weapon equipped, display box and weapon name
-			if ply:GetActiveWeapon() != NULL then
-
-                if playerTeam == "Red" then
-
-                    self.Gradient = surface.GetTextureID("gui/gradient_up")
-                    surface.SetDrawColor(175,20,20, 10 );
-                    surface.SetTexture( self.Gradient );
-                    surface.DrawTexturedRect(0, ScrH()-100, ScrW(), 200);
-
-                    self.Gradient = surface.GetTextureID("gui/gradient_down")
-                    surface.SetDrawColor(175,20,20, 10 );
-                    surface.SetTexture( self.Gradient );
-                    surface.DrawTexturedRect(0, -100, ScrW(), 200);
-
-                    self.Gradient = surface.GetTextureID("gui/gradient_down")
-                    surface.SetDrawColor( 175,20,20, 90 );
-                    surface.SetTexture( self.Gradient );
-                    surface.DrawTexturedRect( 60, ScrH() - 125, 300, 65 );
-                    draw.RoundedBox( 0 , 60, ScrH() - 125, 3, 65, Color(255,255,255,35) )
-                    draw.RoundedBox( 0 , 360, ScrH() - 125, 3, 65, Color(255,255,255,35) )     
-                    
-                    self.Gradient = surface.GetTextureID("gui/gradient_up")
-                    surface.SetDrawColor( 255, 170, 170, 8 );
-                    surface.SetTexture( self.Gradient );
-                    surface.DrawTexturedRect( 60, ScrH() - 95, 300, 35 );
-
-                elseif playerTeam == "Blue" then
-
-                    self.Gradient = surface.GetTextureID("gui/gradient_up")
-                    surface.SetDrawColor(20,20,175, 10 );
-                    surface.SetTexture( self.Gradient );
-                    surface.DrawTexturedRect(0, ScrH()-100, ScrW(), 200);
-
-                    self.Gradient = surface.GetTextureID("gui/gradient_down")
-                    surface.SetDrawColor(20,20,175, 10 );
-                    surface.SetTexture( self.Gradient );
-                    surface.DrawTexturedRect(0, -100, ScrW(), 200);
-
-                    self.Gradient = surface.GetTextureID("gui/gradient_down")
-                    surface.SetDrawColor( 20,20,175, 90 );
-                    surface.SetTexture( self.Gradient );
-                    surface.DrawTexturedRect( 60, ScrH() - 125, 300, 65 );
-                    draw.RoundedBox( 0 , 60, ScrH() - 125, 3, 65, Color(255,255,255,35) )
-                    draw.RoundedBox( 0 , 360, ScrH() - 125, 3, 65, Color(255,255,255,35) )
-                    
-                    self.Gradient = surface.GetTextureID("gui/gradient_up")
-                    surface.SetDrawColor( 170, 170, 255, 8 );
-                    surface.SetTexture( self.Gradient );
-                    surface.DrawTexturedRect( 60, ScrH() - 95, 300, 35 );
-
-                end
-
-				local weaponName = ply:GetActiveWeapon():GetPrintName()
-				draw.DrawText( weaponName, "WeaponShadow", 72 + 1, ScrH() - 122 + 1, Color(0, 0, 0, 255), TEXT_ALIGN_LEFT ) 
-				draw.DrawText( weaponName, "Weapon", 72, ScrH() - 122, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT ) 
-
-                local commandText = "[F1] Switch Teams - [F2] Change Classes"
-                draw.DrawText( commandText, "CommandsShadow", 60 + 11, ScrH()-78 + 1, Color(0, 0, 0, 255), TEXT_ALIGN_LEFT ) 
-                draw.DrawText( commandText, "Commands", 60 + 10, ScrH()-78 , Color(255, 255, 255, 255), TEXT_ALIGN_LEFT ) 
-
-			end
-
-            -- AMMO #, if valid
-
-			if ( IsValid( LocalPlayer():GetActiveWeapon() ) ) then
-
-				local ammoLeft = LocalPlayer():GetActiveWeapon():Clip1()
-
-			    local ammoTotal = LocalPlayer():GetAmmoCount(LocalPlayer():GetActiveWeapon():GetPrimaryAmmoType())
-
-                if ammoLeft != -1 then
-
-                    draw.RoundedBox( 0 , 295, ScrH() - 120, 2, 35, Color(255,255,255,10) )
-
-                    draw.DrawText( ammoTotal, "AmmoSmallShadow", 326 + 1, ScrH() - 121 + 1, Color(0, 0, 0, 250), TEXT_ALIGN_CENTER ) 
-                    draw.DrawText( ammoTotal, "AmmoSmall", 326, ScrH() - 121, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER ) 
-
-                    draw.DrawText( ammoLeft, "AmmoLargeShadow", 260 + 1, ScrH() - 126 + 1, Color(0, 0, 0, 255), TEXT_ALIGN_CENTER ) 
-                    draw.DrawText( ammoLeft, "AmmoLarge", 260, ScrH() - 126, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER ) 
-
-                end
-			end
 		end
 
 	end
@@ -303,30 +335,6 @@ function GM:HUDShouldDraw( name )
     return true
 
 end
-
-function NamesOverPlayers()
-
-    --If spectating, show names over all active players heads
-    if LocalPlayer():Team() == TEAM_SPEC then 
-
-        for k, v in pairs(player.GetAll()) do
-
-            if v:IsValid() && v:Alive() && v:Team() != TEAM_SPEC then
-                
-                local pos = (v:GetPos() + Vector(0,0,80)):ToScreen()
-                
-                draw.SimpleText( v:Nick(), "CommandsBlur", pos.x + 1, pos.y + 1, team.GetColor(v:Team()), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-                draw.SimpleText( v:Nick(), "CommandsShadow", pos.x + 1, pos.y + 1, Color( 0, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-                draw.SimpleText( v:Nick(), "Commands", pos.x, pos.y, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-                
-            end
-
-        end
-    
-    end
-
-end
-hook.Add("HUDPaint", "PlayerNamesForSpectators", NamesOverPlayers)
 
 hook.Add( "PostDrawOpaqueRenderables", "PlayerNamesOverHeadsYeah", function()
 
